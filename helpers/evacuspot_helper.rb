a5d974dfaa67d868f02b29feaef45073e7bd07f5
Sia <siakaramalegos@gmail.com>
2016-05-14 15:21:13 -0500

    Adds todo to switch device geolocation

--- helpers/evacuspot_helper.rb
+++ helpers/evacuspot_helper.rb
@@ -1,6 +1,7 @@
 module EvacuspotHelper
 
   # Get geo-location using IP address of device
+  # TODO: switch to use navigator.geolocation.getCurrentPosition as primary position-finder then IP address as back-up
   def set_device_location(ip_address)
     uri = "http://freegeoip.net/json/#{ip_address}"
     location_response = HTTParty.get(uri)
